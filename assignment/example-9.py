da3a22211660c2c6ddd6aa321c95f73aab28edcd
netrapanchal31-cmd <netrapanchal31@gmail.com>
2026-02-23 21:18:45 +0530

    Add files via upload

---
+++ assignment/example-9.py
@@ -0,0 +1,5 @@
+#Write a Python program to find the second smallest number in a list.  
+lst_number=[1,4,8,3,9,12,2,90]
+lst_number.sort()
+print(lst_number)
+
---
+++ assignment/example.py
@@ -0,0 +1,6 @@
+number01=int(input("enter a number"))
+ans=0
+for i in range(1, number01+1):
+    ans=ans+i 
+
+print("sum of",number01, "is", ans)
