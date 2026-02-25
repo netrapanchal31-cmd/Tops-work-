74330352549c9223f4976b6bc7cf50f6f82c9358
netrapanchal31-cmd <netrapanchal31@gmail.com>
2026-02-25 10:05:36 +0530

    Add files via upload

---
+++ projects/ppc-1.py
@@ -0,0 +1,6 @@
+#Find Ared of circle (pi*radius*radius) 
+#  note : pi = 3.14 , radius=accept float value from user
+pi=3.14
+r=int(input("enter a radius"))
+area=pi*r*r
+print("cicle of area=", area)
