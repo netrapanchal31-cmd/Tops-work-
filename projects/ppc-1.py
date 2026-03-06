ba6462dde57f25abd04b858ac56e5139f6856b77
netrapanchal31-cmd <netrapanchal31@gmail.com>
2026-03-06 21:14:32 +0530

    Delete projects directory

--- projects/ppc-1.py
+++
@@ -1,6 +0,0 @@
-#Find Ared of circle (pi*radius*radius) 
-#  note : pi = 3.14 , radius=accept float value from user
-pi=3.14
-r=int(input("enter a radius"))
-area=pi*r*r
-print("cicle of area=", area)
--- projects/ppc-2.py
+++
@@ -1,5 +0,0 @@
-#Find maximum number using max function.
-num1=int(input("enter a number"))
-num2=int(input("enter a second number"))
-maximum=max(num1,num2)
-print("maximum number among both is = ", maximum)
