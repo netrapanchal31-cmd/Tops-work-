935faf503fadc6a6aa6bdc38671763b954f0a5b8
netrapanchal31-cmd <netrapanchal31@gmail.com>
2026-03-06 21:19:22 +0530

    Add files via upload

---
+++ PYTHON/assignments/example-2.py
@@ -0,0 +1,4 @@
+statement=input("enter a statement")
+substring=input("enter a substring")
+count=statement.count(substring)
+print(count)
---
+++ PYTHON/assignments/example-6.py
@@ -0,0 +1,4 @@
+word=input("enter a statement ")
+new_word=""
+new_word=word.replace("not poor","good")
+print(new_word)
