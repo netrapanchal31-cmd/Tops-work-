e9dfe393e73f3797d46e1ca2733fe8fb914d537e
netrapanchal31-cmd <netrapanchal31@gmail.com>
2026-03-16 21:43:06 +0530

    Delete PYTHON/assignments directory

--- PYTHON/assignments/example-2.py
+++
@@ -1,4 +0,0 @@
-statement=input("enter a statement")
-substring=input("enter a substring")
-count=statement.count(substring)
-print(count)
--- PYTHON/assignments/example-9.py
+++
@@ -1,5 +0,0 @@
-#Write a Python program to find the second smallest number in a list.  
-lst_number=[1,4,8,3,9,12,2,90,8927,5,17,89,99]
-lst_number.sort()
-print("The second smallest number in a list: ",lst_number[1])
-
